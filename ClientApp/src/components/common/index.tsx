b6aade1b34e7fd25f19dfe43d0ea44919603e00a
mazurski20 <74248381+mazurski20@users.noreply.github.com>
2023-10-30 19:32:22 -0400

    Auth0 edits made to index.tsx and .env

--- ClientApp/src/components/common/index.tsx
+++ ClientApp/src/components/common/index.tsx
@@ -4,11 +4,21 @@ import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import "bootstrap/dist/css/bootstrap.css";
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 
+const domain = process.env.REACT_APP_AUTH0_DOMAIN as string;
+const clientId = process.env.REACT_APP_AUTH0_CLIENTID as string;
 const container = document.getElementById("app-root")!;
 const root = createRoot(container);
+
 root.render(
   <Router>
-    <App />
+      <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        authorizationParams={{redirect_uri: window.location.origin}}
+      >
+        <App />
+      </Auth0Provider>
   </Router>
 );
