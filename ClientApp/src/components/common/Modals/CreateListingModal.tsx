0f66c149f4b2bf9d8d7156ab3e63857ab98f255a
RockosBos <32206608+RockosBos@users.noreply.github.com>
2023-11-05 22:17:26 -0500

    Modal Corrections

--- ClientApp/src/components/common/Modals/CreateListingModal.tsx
+++ ClientApp/src/components/common/Modals/CreateListingModal.tsx
@@ -14,21 +14,22 @@ export default function CreateListingModal(props: ModalType){
 				<div className="overlay">
 					<div className="box">
 						<form className="create-listing-form">
+							<h1>Please Enter Listing Information:</h1>
 							<label>
-								Title:
-								<input type="text" name="title" />
-							</label>
-							<label>
-								Description:
-								<textarea name="description" />
-							</label>
-							<label>
-								Location:
+								<p>Location:</p>
 								<input type="text" name="location" />
 							</label>
 							<label>
-								Rate:
+								<p>Rate:</p>
 								<input type="text" name="rate" />
+							</label>
+							<label>
+								<p>Relevant Skills:</p>
+								<input type="text" name="title" />
+							</label>
+							<label>
+								<p>Description:</p>
+								<textarea name="description" />
 							</label>
 							<div>
 								<button >Create</button>
