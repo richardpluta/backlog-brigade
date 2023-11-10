b1a708ee78ba96f18f0a3a4101ce93de3bb3ae6e
RockosBos <32206608+RockosBos@users.noreply.github.com>
2023-11-10 00:01:26 -0500

    In progress api changes

--- ClientApp/src/components/common/Modals/CreateListingModal.tsx
+++ ClientApp/src/components/common/Modals/CreateListingModal.tsx
@@ -1,4 +1,5 @@
-import React, { ReactNode, useState } from "react";
+import React, { ReactNode, useState, MouseEvent, FormEvent } from "react";
+import listing from "../../../models/listingData";
 import "./CreateListingModal.css";
 
 
@@ -8,32 +9,75 @@ interface ModalType {
 	toggle: () => void;
 }
 
+const DUMMY_USER = [
+	{
+		userID: 123,
+		userType: 0,
+		userName: "DummyUserFromModal",
+		phone: 0,
+		email: "nam@email.com",
+		skillset: 0,
+		zip: 12345,
+		userRate: 0,
+	}
+]
+
+
 export default function CreateListingModal(props: ModalType){
+	const onSubmit = (event: any) => {
+		event.preventDefault();
+
+		const target = event.target;
+
+		const data = {
+			id: 123,
+			userId: 0,
+			postDate: "2023-11-10T04:41:44.124Z",
+			postContent: target.description.value,
+			flagged: false,
+			skillSet: Number(target.skills.value),
+			expectedRate: Number(target.rate.value),
+			user: DUMMY_USER,
+		}
+
+		fetch("api/listing",
+			{
+				method: 'POST',
+				headers: {
+					'Accept': 'application/json, text/plain',
+					'Content-Type': 'application/json;charset=UTF-8'
+				},
+				body: JSON.stringify(data)
+			}
+		).then(res => {
+			console.log(res.json)
+		});
+	}
 	return(
 		<>
 			{props.isOpen && (
 				<div className="overlay">
 					<div className="box">
-						<form className="create-listing-form">
+						<form className="create-listing-form" onSubmit={onSubmit}>
 							<h1>Please Enter Listing Information:</h1>
-							<label>
-								<p>Location:</p>
-								<input type="text" name="location" />
-							</label>
-							<label>
-								<p>Rate:</p>
-								<input type="text" name="rate" />
-							</label>
-							<label>
-								<p>Relevant Skills:</p>
-								<input type="text" name="title" />
-							</label>
-							<label>
-								<p>Description:</p>
-								<textarea name="description" />
-							</label>
+							<div className="field">
+								<label htmlFor="location">Location:</label>
+								<input id="location" />
+							</div>
+							<div className="field">
+								<label htmlFor="rate">Rate:</label>
+								<input id="rate" />
+							</div>
+							<div className="field">
+								<label htmlFor="skills">Relevant Skills:</label>
+								<input id="skills" />
+							</div>
+							<div className="field">
+								<label htmlFor="description">Description:</label>
+								<textarea name="description" id="description"/>
+							</div>
 							<div>
-								<button >Create</button>
+								<button>Create</button>
 								<button onClick={props.toggle}>Cancel</button>
 							</div>
 						</form>
