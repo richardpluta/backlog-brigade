9f63f28f67adbac744256925e40df8f2d27062da
RockosBos <32206608+RockosBos@users.noreply.github.com>
2023-11-20 15:44:54 -0500

    Edits to help wanted modal

--- ClientApp/src/components/common/Hooks/useCreateHelpWantedModal.tsx
+++ ClientApp/src/components/common/Hooks/useCreateHelpWantedModal.tsx
@@ -3,9 +3,16 @@ import { useState } from "react";
 export default function useCreateHelpWantedModal() {
   const [isOpen, setisOpen] = useState(false);
 
-  const toggle = () => {
-    setisOpen(!isOpen);
-  };
+  	const toggle = () => {
+		if(!isOpen){
+			document.body.style.overflow = 'hidden';
+		}
+		else{
+			document.body.style.overflow = 'unSet';
+		}
+		window.scrollTo({top: 0});
+		setisOpen(!isOpen);
+	}
 
   return {
     isOpen,
